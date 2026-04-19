b436a09af194335cda927eed5a244b444193b47a
malbasini <malbasini@gmail.com>
2026-04-19 04:26:53 +0200

    REDIRECT HTTP TO HTTPS

--- src/main/java/com/example/demo/config/WebAppInitializer.java
+++ src/main/java/com/example/demo/config/WebAppInitializer.java
@@ -1,22 +1,43 @@
 package com.example.demo.config;
 
 import javax.servlet.Filter;
+import javax.servlet.ServletRegistration;
+import javax.servlet.ServletSecurityElement;
+import javax.servlet.HttpConstraintElement;
+import javax.servlet.annotation.ServletSecurity;
+
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-    @Override protected Class<?>[] getRootConfigClasses() {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
         return new Class<?>[]{ AppRootConfig.class, SecurityConfig.class };
     }
-    @Override protected Class<?>[] getServletConfigClasses() {
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
         return new Class<?>[]{ WebMvcConfig.class };
     }
-    @Override protected String[] getServletMappings() { return new String[]{ "/" }; }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{ "/" };
+    }
 
     @Override
     protected Filter[] getServletFilters() {
-        // Aggiungiamo il DelegatingFilterProxy
         return new Filter[]{
                 new org.springframework.web.filter.DelegatingFilterProxy("springSecurityFilterChain")
         };
+    }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setServletSecurity(
+                new ServletSecurityElement(
+                        new HttpConstraintElement(ServletSecurity.TransportGuarantee.CONFIDENTIAL)
+                )
+        );
     }
 }
